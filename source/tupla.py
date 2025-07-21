54215c78bd00867557abda9473e83e705fcbfaee
Henrique Ventura <trycode2018@gmail.com>
2025-07-21 01:02:23 +0100

    termino do estudo pra hoje

---
+++ source/tupla.py
@@ -0,0 +1,6 @@
+dias_semana = ('Domingo','Segunda-feira','Terca-feira','Quarta-feira',
+               'Quinta-feira','Sexta-feira','Sabado')
+print(type(dias_semana))
+lista = list(dias_semana)
+lista[1] = 'Segunda'
+print(lista)
